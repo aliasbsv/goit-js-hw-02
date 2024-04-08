858f83fc4b6d2992f2d107f0ff4119eb8acf1d8d
aliasbsv <aliasbsv@gmail.com>
2024-04-08 14:40:47 +0200

    mes

--- js/task-3.js
+++ js/task-3.js
@@ -1,10 +1,17 @@
-function getElementWidth(content, padding, border) { 
-  cont = Number.parseFloat(content);
-  pad = Number.parseFloat(padding);
-  bord = Number.parseFloat(border);
-  const totalWeight = cont + (pad * 2) + (bord * 2);
-  return  totalWeight;
+function checkForSpam(message) { 
+  let mesNeu = message.toLowerCase();
+  if ((mesNeu.includes('spam')) || (mesNeu.includes('sale'))) {
+    return true;
+  }
+  else { 
+    return false;
+  }
 }
-      console.log(getElementWidth("50px", "8px", "4px")); // 74
-      console.log(getElementWidth("60px", "12px", "8.5px")); // 101
-      console.log(getElementWidth("200px", "0px", "0px")); // 200
+
+console.log(checkForSpam("Latest technology news")); // false
+console.log(checkForSpam("JavaScript weekly newsletter")); // false
+console.log(checkForSpam("Get best sale offers now!")); // true
+console.log(checkForSpam("Amazing Sale, only tonight!")); // true
+console.log(checkForSpam("Trust me, this is not a spam message")); // true
+console.log(checkForSpam("Get rid of sPaM emails. Our book in on sale!")); // true
+console.log(checkForSpam("[SPAM] How to earn fast money?")); // true
